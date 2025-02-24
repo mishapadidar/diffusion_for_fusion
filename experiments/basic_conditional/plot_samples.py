2a2d79c7c8a62fe33152fa0496f57902e5d98105
mishapadidar <mishapadidar14@gmail.com>
2025-02-24 11:04:11 -0500

    minor

--- experiments/basic_conditional/plot_samples.py
+++ experiments/basic_conditional/plot_samples.py
@@ -16,8 +16,11 @@ plt.rcParams.update({'font.size': 14})
 colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", 
           "tab:brown", "tab:pink", "tab:gray", "tab:olive", "tab:cyan"]
 
-# full dimension data
-indir = "./output/mean_iota_aspect_ratio/run_uuid_4123533c-960a-411b-9ff0-2a990e3eb305"
+
+# # conditioned on (iota, aspect) for nfp=4, helicity=1;  trained on PCA-9
+# indir = "./output/mean_iota_aspect_ratio/run_uuid_4123533c-960a-411b-9ff0-2a990e3eb305"
+# conditioned on (iota, aspect, nfp, helicity); trained on PCA-9
+indir = "output/mean_iota_aspect_ratio_nfp_helicity/run_uuid_da5a3230-deca-4709-a775-76b7365fbbd2"
 
 config_pickle = indir+"/config.pickle"
 model_path = indir+"/model.pth"
@@ -41,11 +44,16 @@ print(np.shape(X_train))
 # subset some data
 Y_init = from_standard(Y_train, Y_mean, Y_std)
 # idx_subset = ((np.abs(Y_init[:,0] - 1.1)/1.1 < 0.001) & (np.abs(Y_init[:,1] - 12)/12 < 0.01)) # fig 9
-idx_subset = ((np.abs(Y_init[:,0]- 2.30)/2.30 < 0.001) & (np.abs(Y_init[:,1] - 12)/12 < 0.1)) # fig 11
-
+# idx_subset = ((np.abs(Y_init[:,0]- 2.30)/2.30 < 0.001) & (np.abs(Y_init[:,1] - 12)/12 < 0.1)) # fig 11
+# idx_subset = ((np.abs(Y_init[:,0] - 1.1)/1.1 < 0.001) & (np.abs(Y_init[:,1] - 12)/12 < 0.01) & 
+#               (Y_init[:,2] == 4) & (Y_init[:,3] == 1)) # fig 9
+idx_subset = ((np.abs(Y_init[:,0]- 2.30)/2.30 < 0.001) & (np.abs(Y_init[:,1] - 12)/12 < 0.1) & 
+              (Y_init[:,2] == 4) & (Y_init[:,3] == 1)) # fig 11
 
 X_train = X_train[idx_subset]
 Y_train = Y_train[idx_subset]
+print(np.shape(X_train))
+
 
 # generate conditions from data
 n_samples = 256
@@ -55,13 +63,24 @@ cond_eval = generate_conditions_for_eval(Y_train, batch_size = n_samples, from_t
 samples =diffusion.sample(cond_eval)
 samples = samples.cpu().detach().numpy()
 
+
+# plot the location of the samples in Y-space
+Y_train = from_standard(Y_train, Y_mean, Y_std)
+cond_eval = from_standard(cond_eval, Y_mean, Y_std)
+plt.scatter(Y_train[:,0], Y_train[:,1], alpha=0.6, label='actual')
+plt.scatter(cond_eval[:,0], cond_eval[:,1], alpha=0.6, label='diffusion')
+plt.xlabel('mean rotational transform')
+plt.ylabel('aspect ratio')
+plt.legend(loc='upper right')
+plt.show()
+
 # PCA for plotting in 2D data
 pca_plot = PCA(n_components=2, svd_solver='full')
 X_2d = pca_plot.fit_transform(X_train)
 samples_2d = pca_plot.transform(samples)
 
-plt.scatter(X_2d[:,0], X_2d[:,1], label='actual')
-plt.scatter(samples_2d[:,0], samples_2d[:,1], label='diffusion')
+plt.scatter(X_2d[:,0], X_2d[:,1], alpha=0.6, label='actual')
+plt.scatter(samples_2d[:,0], samples_2d[:,1], alpha=0.6, label='diffusion')
 plt.legend(loc='upper right')
 plt.show()
 
--- experiments/basic_conditional/train.sh
+++ experiments/basic_conditional/train.sh
@@ -1,4 +1,7 @@
 # train on pca data
-python train_model.py --conditions 'mean_iota' 'aspect_ratio' --train_batch_size 640 --num_epochs 500 --learning_rate 5e-4 --num_timesteps 200 --input_emb_size 64 --time_emb_size 128 --cond_emb_size 128 --hidden_size 512  --hidden_layers 3 --time_emb_type "sinusoidal" --use_pca --pca_size 9 --save_images_step 10
+# python train_model.py --conditions 'mean_iota' 'aspect_ratio' --train_batch_size 4096 --num_epochs 500 --learning_rate 5e-4 --num_timesteps 200 --input_emb_size 64 --time_emb_size 128 --cond_emb_size 128 --hidden_size 512  --hidden_layers 3 --time_emb_type "sinusoidal" --use_pca --pca_size 9 --save_images_step 10
+python train_model.py --conditions 'mean_iota' 'aspect_ratio' 'nfp' 'helicity' --train_batch_size 4096 --num_epochs 150 --learning_rate 5e-4 --num_timesteps 200 --input_emb_size 64 --time_emb_size 128 --cond_emb_size 128 --hidden_size 1024  --hidden_layers 3 --time_emb_type "sinusoidal" --use_pca --pca_size 9 --save_images_step 10
+
 # train on all dimensions
-# python train_model.py --conditions 'mean_iota' 'aspect_ratio' --train_batch_size 512 --num_epochs 250 --learning_rate 5e-4 --num_timesteps 200 --input_emb_size 64 --time_emb_size 64 --cond_emb_size 64 --hidden_size 1024  --hidden_layers 5 --time_emb_type "sinusoidal" --save_images_step 10
+# python train_model.py --conditions 'mean_iota' 'aspect_ratio' 'nfp' 'helicity' --train_batch_size 4096 --num_epochs 250 --learning_rate 5e-4 --num_timesteps 400 --input_emb_size 64 --time_emb_size 128 --cond_emb_size 128 --hidden_size 4096  --hidden_layers 3 --time_emb_type "sinusoidal" --save_images_step 10
+
