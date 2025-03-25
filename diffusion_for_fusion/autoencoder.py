f3122428e0b7afa86323233f06a7faabdb3d9a48
mishapadidar <mishapadidar14@gmail.com>
2025-03-24 22:44:01 -0400

    new autoencoder architecture with superior performance and a test script

--- diffusion_for_fusion/autoencoder.py
+++ diffusion_for_fusion/autoencoder.py
@@ -11,25 +11,40 @@ class EncoderBlock(nn.Module):
 
     def forward(self, x: torch.Tensor):
         return self.act(self.ff(x))
+    
+class ResidualBlock(nn.Module):
+    def __init__(self, size_in: int, size_out: int):
+        super().__init__()
+
+        self.ff = nn.Linear(size_in, size_out)
+        self.act = nn.GELU()
+        self.residual = nn.Linear(size_in, size_out)
+
+    def forward(self, x: torch.Tensor):
+        return self.act(self.ff(x) + self.residual(x))
 
 # Defining Autoencoder model
 class Autoencoder(nn.Module):
-   def __init__(self, input_size, encoding_size, base=2):
+    def __init__(self, input_size, encoding_size, base=2):
         super().__init__()
 
         # layer sizes
         sizes = base_layer_sequence(input_size, encoding_size, base=base)
-        print(sizes)
         layers = [EncoderBlock(sizes[ii], sizes[ii-1]) for ii in range(len(sizes)-1,0,-1)]
         self.encoder = nn.Sequential(*layers)
 
         layers = [EncoderBlock(sizes[ii], sizes[ii+1]) for ii in range(len(sizes)-1)]
         self.decoder = nn.Sequential(*layers)
 
-   def forward(self, x):
-       x = self.encoder(x)
-       x = self.decoder(x)
-       return x
+        self.encoder_residual = nn.Linear(input_size, encoding_size)
+        self.decoder_residual = nn.Linear(encoding_size, input_size)
+
+    def forward(self, x0):
+        # encode
+        x1 = self.encoder(x0) + self.encoder_residual(x0)
+        # decode
+        x2 = self.decoder(x1) + self.decoder_residual(x1)
+        return x2
    
 def base_layer_sequence(input_size, encoding_size, base=2):
     """Makes a base-b list of layer sizes.
@@ -46,3 +61,18 @@ def base_layer_sequence(input_size, encoding_size, base=2):
     kmax = floor(log(input_size, base))
     layers = [encoding_size, *[base**k for k in range(kmin, kmax+1)], input_size]
     return layers
+
+def init_autoencoder_from_config(config, input_dim):
+    """Initialize an autoencoder from a config file.
+
+    Args:
+        config (argparse.Namespace): A config file that has been ingested by argparse,
+            config = parser.parse_args()
+        input_dim (int): dimension of X_train
+
+    Returns:
+        autoenc: initialized autoencoder
+    """
+    autoenc = Autoencoder(input_dim, config.encoding_size, base=config.encoding_base)
+    return autoenc
+
--- experiments/latent_diffusion/train_autoencoder.sh
+++ experiments/latent_diffusion/train_autoencoder.sh
@@ -1,2 +1,7 @@
 # train autoencoder
-python train_autoencoder.py --train_batch_size 4096 --num_epochs 300 --learning_rate 5e-4  --encoding_size 5 --encoding_base 6
+
+# bigger encoding size
+# python train_autoencoder.py --train_batch_size 4096 --num_epochs 200 --learning_rate 5e-4  --encoding_size 50 --encoding_base 4
+
+# smaller encoding size
+# python train_autoencoder.py --train_batch_size 4096 --num_epochs 200 --learning_rate 5e-4  --encoding_size 5 --encoding_base 6
