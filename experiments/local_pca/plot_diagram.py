e5293d0ba91a70626793cf6b45856cb89b6f6a5a
mishapadidar <mishapadidar14@gmail.com>
2025-05-13 13:24:20 -0400

    added cdf to diagram

--- experiments/local_pca/plot_diagram.py
+++ experiments/local_pca/plot_diagram.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from experiments.basic_conditional.load_quasr_data import load_quasr_data
+from scipy.stats import gaussian_kde
 
 
 """ 
@@ -34,7 +35,7 @@ X_train = X_train[idx_keep]
 fX_train = fX_train[idx_keep]
 
 
-fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+fig, axes = plt.subplots(1, 3, figsize=(10, 5))
 
 # contour plot of the objective
 x = np.linspace(lb, ub, 100)
@@ -67,17 +68,45 @@ for ii in range(2):
 pca = PCA(n_components=1)
 X_train_pca = pca.fit_transform(X_train)
 X_train_pca = pca.inverse_transform(X_train_pca)
-axes[1].scatter(X_train_pca[:, 0], X_train_pca[:, 1], color='tab:blue', alpha=0.5)
+axes[1].scatter(X_train_pca[:, 0], X_train_pca[:, 1], color='tab:orange', alpha=0.5)
 
 for ii in range(2):
     axes[ii].set_xlim(lb,ub)
     axes[ii].set_ylim(lb,ub)
-    axes[ii].legend(loc='lower right')
+    # axes[ii].legend(loc='lower right')
     axes[ii].set_xticks([])
     axes[ii].set_yticks([])
 
 axes[0].set_title("Samples")
 axes[1].set_title("Samples After Local PCA")
+
+
+# generate more data for the histogram
+X_train = np.random.uniform(low=lb, high=ub, size=(50000, 2))
+fX_train = f(X_train[:, 0], X_train[:, 1])
+idx_keep = fX_train < f_cutoff
+X_train = X_train[idx_keep]
+fX_train = fX_train[idx_keep]
+pca = PCA(n_components=1)
+X_train_pca = pca.fit_transform(X_train)
+X_train_pca = pca.inverse_transform(X_train_pca)
+fX_train_pca = f(X_train_pca[:, 0], X_train_pca[:, 1])
+
+# Compute and plot the CDF of the objective value
+sorted_fX_train = np.sort(fX_train)
+cdf_pca = np.arange(1, len(sorted_fX_train) + 1) / len(sorted_fX_train)
+axes[2].plot(sorted_fX_train, cdf_pca, color='tab:blue', lw=2, linestyle='dashed', label='Samples')
+
+sorted_fX_train_pca = np.sort(fX_train_pca)
+cdf_pca = np.arange(1, len(sorted_fX_train_pca) + 1) / len(sorted_fX_train_pca)
+axes[2].plot(sorted_fX_train_pca, cdf_pca, color='tab:orange', lw=2, linestyle='dashdot', label='Local PCA')
+
+axes[2].grid(True, which='both', linestyle='-', linewidth=1, alpha=0.4, color='gray', zorder=0)
+axes[2].set_title("Objective Value CDF")
+# axes[2].set_xlabel("Objective Value")
+# axes[2].set_ylabel("Probability")
+axes[2].legend(loc='lower right')
+
 
 plt.tight_layout()
 fig.savefig("local_pca_diagram.pdf", format="pdf", bbox_inches="tight")
