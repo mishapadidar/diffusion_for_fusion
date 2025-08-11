457ae97b861868a4cadedf077bec596122f12262
mishapadidar <mishapadidar14@gmail.com>
2025-08-11 10:06:33 -0400

    removing latent diffusion

--- experiments/latent_diffusion/train_conditional_autoencoder.sh
+++
@@ -1,4 +0,0 @@
-# train conditional autoencoder
-
-# smaller encoding size
-python train_conditional_autoencoder.py --conditions 'mean_iota' 'aspect_ratio' 'nfp' 'helicity' --train_batch_size 4096 --num_epochs 200 --learning_rate 5e-4  --encoding_size 5 --encoding_base 4 --cond_emb_size 64
